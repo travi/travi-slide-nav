21f0dc70a3f45c87967d258cfd91e50818262d5f
Matt Travi <programmer@travi.org>
2014-05-06 08:50:46 -0500

    moved aliases to aliases file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,4 @@ module.exports = function (grunt) {
     'use strict';
 
     require('load-grunt-config')(grunt);
-
-    grunt.registerTask('gates', ['jslint', 'karma']);
-    grunt.registerTask('default', 'gates');
 };
